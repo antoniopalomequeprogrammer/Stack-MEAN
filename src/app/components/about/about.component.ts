b2038f723be717bbf86971775b1e822dab58bf98
=antoniopalomequeprogrammer <palomequework@gmail.com>
2021-05-03 17:42:17 +0200

    Proyecto stack MEAN

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  public title:string;
+  public subtitle:string;
+  public email:string;
+
+  constructor() {
+  
+    this.title = "Antonio Luis Palomeque"
+    this.subtitle ="Desarrollador Web "
+    this.email = "palomequework@gmail.com"
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
